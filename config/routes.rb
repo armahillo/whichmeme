c2e2c7e85af64a65ca615ac92847b99b4e898ab9
Aaron (Home) <armahillo@gmail.com>
2015-11-23 17:22:06 -0500

    Tweaks the view codes and adds a different home route

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root to:'memes#index'
+  root to:'application#index'
 
   resources :memes, only: [:index, :show]
 
