66296a2e7748491de81722a4384219c0c562b516
Aaron (Home) <armahillo@gmail.com>
2015-11-23 17:30:32 -0500

    Updates unicorn to mimic dailyspitpaint

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,12 @@
-# set path to application
-#root = "/home/deploy/apps/whichmeme"
-#app_dir = root + "/current"
-#shared_dir = root + "/shared"
+app_name = 'whichmeme'
+environment = ENV['RACK_ENV'] || ENV['RAILS_ENV'] || "production"
+app_path = "/home/deploy/apps/whichmeme"
+bundle_path = "/home/deploy/apps/whichmeme/shared/vendor/bundle/ruby/2.2.0"
 
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-working_directory app_dir
+# set path to application
+#app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_path}/shared"
+working_directory "#{app_path}/current"
 
 
 # Set unicorn options
@@ -13,25 +14,45 @@ worker_processes 4
 preload_app true
 timeout 30
 
-before_fork do |server, worker|
-  # ...
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
-end
-
-after_fork do |server, worker|
-  # ...
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
-end
-
-
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", backlog: 64
+listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen(3000, backlog: 64) if ENV['RAILS_ENV'] == 'development'
 
 # Logging
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
 stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 # Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
+if ENV['RAILS_ENV'] == 'production'
+  pid "#{shared_dir}/pids/unicorn.pid" 
+elsif ENV['RAILS_ENV'] == 'development'
+  pid "/tmp/#{app_name}.pid" 
+end
+
+Unicorn::HttpServer::START_CTX[0] = "#{bundle_path}/bin/unicorn"
+Unicorn::Configurator::DEFAULTS[:logger].formatter = Logger::Formatter.new
+
+before_exec do |_|
+  paths = (ENV["PATH"] || "").split(File::PATH_SEPARATOR)
+  paths.unshift "#{bundle_path}/bin"
+
+  ENV["PATH"] = paths.uniq.join(File::PATH_SEPARATOR)
+  ENV["GEM_HOME"] = ENV['GEM_PATH'] = bundle_path
+  ENV["BUNDLE_GEMFILE"] = "#{app_path}/current/Gemfile"
+end
+
+# Garbage collection settings.
+GC.respond_to?(:copy_on_write_friendly=) &&
+  GC.copy_on_write_friendly = true
+
+# If using ActiveRecord, disconnect (from the database) before forking.
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+end
+
+# After forking, restore your ActiveRecord connection.
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.establish_connection
+end
