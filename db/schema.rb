2702041a02c6e1ea5c06b1e32de655979058b499
Aaron Hill <armahillo@gmail.com>
2016-03-18 08:42:03 -0400

    Creates News model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118191201) do
+ActiveRecord::Schema.define(version: 20160318124143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,13 @@ ActiveRecord::Schema.define(version: 20160118191201) do
     t.integer  "correct_meme_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "news", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_auths", force: :cascade do |t|
