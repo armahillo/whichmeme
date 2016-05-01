6fdcf828466b200696f3095e1fc748d54b9cb68b
Aaron Hill <armahillo@gmail.com>
2016-05-01 13:41:15 -0400

    Grabs avatar even if the user already has a record.

--- app/models/user.rb
+++ app/models/user.rb
@@ -78,18 +78,21 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
-  	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  	u = self.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
   		user.email = auth.info.email
   		user.password = Devise.friendly_token[0,20]
   		user.name = auth.info.name
       user.fake_name = (Faker::Faker.name + ", " + Faker::Faker.suffix) unless user.fake_name.present? 
   		user.avatar_url = auth.info.try(:image) rescue ""
   	end
+    # If they haven't provided an avatar image, let's see if we can get one this time.
+    u.avatar_url ||= auth.info.try(:image) rescue nil
+    return u
   end
 
   def self.new_with_session(params, session)
   	super.tap do |user|
-      user.fake_name = (Faker::Faker.name + ", " + Faker::Faker.suffix) unless user.fake_name.present? 
+      user.fake_name ||= (Faker::Faker.name + ", " + Faker::Faker.suffix)
 
   		if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
   			user.email = data["email"] unless user.email.present?
